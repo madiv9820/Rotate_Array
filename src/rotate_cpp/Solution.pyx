f8812d3f67ac39a58ccc8b659473d55a9b449a05
madiv9820 <themathur901@gmail.com>
2025-12-23 14:49:03 +0530

    Wrapper for c++ code

---
+++ src/rotate_cpp/Solution.pyx
@@ -0,0 +1,29 @@
+# distutils: language = c++
+
+from libcpp.vector cimport vector
+
+cdef extern from "Solution.hpp":
+    cdef cppclass Solution:
+        Solution() except + 
+        void rotate(vector[int]& nums, int k)
+    
+cdef class cppSolution:
+    cdef Solution* thisptr
+
+    def __cinit__(self):
+        self.thisptr = new Solution()
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    def rotate(self, nums, int k):
+        cdef vector[int] cpp_arr
+        cdef int i
+        for i in nums:
+            cpp_arr.push_back(i)
+
+        self.thisptr.rotate(cpp_arr, k)
+
+        nums.clear()
+        for i in range(cpp_arr.size()):
+            nums.append(cpp_arr[i])
